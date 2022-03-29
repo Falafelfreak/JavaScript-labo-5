3c7fc6313772bdc6991c915ce6e7836beb43f0ea
Falafelfreak <lukaswackenier@gmail.com>
2022-03-29 21:24:10 +0200

    Initial commit

---
+++ opdracht de en het/scripts/code.js
@@ -0,0 +1,16 @@
+const setup = () => {
+    let zin = "Gisteren zat de jongen op de stop en at de helft van de appel.";
+    let output = "";
+    for(let i =0; i < zin.length; i++){
+        if(zin.substring(i-1,i+3)===" de "){
+            output += "het ";
+            i = i+2;
+        }
+        else{
+        output += zin.substring(i,i+1);
+        }
+    }
+    console.log(output);
+}
+
+window.addEventListener("load", setup);
